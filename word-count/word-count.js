7b3f32d50adda8227b8b1ef33decd8f18a98578b
Amit Kumar <amit00272@hotmail.com>
2017-02-19 19:28:52 +0530

    commit after delete

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -1,3 +1,15 @@
-/**
- * Created by Amit on 19-02-2017.
- */
+
+var Word=function () {
+    this.number=10;
+}
+
+Word.prototype.count = function(pGraphs){
+
+var v=pGraphs.trim().split(/[ ,]+/);
+var c=function () {}
+c.setAttribute("test","5");
+console.log(c)
+}
+
+new Word().count("one two,three")
+module.exports=Word;
