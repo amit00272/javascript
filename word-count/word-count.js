012a714c09f44827a5d7be7493d90fdac53b14d9
Amit Kumar <amit00272@hotmail.com>
2017-02-19 23:33:55 +0530

    commit after delete

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -1,15 +1,17 @@
 
-var Word=function () {
-    this.number=10;
-}
+var Word=function () {}
 
 Word.prototype.count = function(pGraphs){
 
-var v=pGraphs.trim().split(/[ ,]+/);
-var c=function () {}
-c.setAttribute("test","5");
-console.log(c)
+var ocr={};
+pGraphs=pGraphs.toLowerCase();
+    var ocr={};
+    var regX=/\b[a-z\u0000-\u00FF0-9A-Z\']+\b/g;
+    while(gp = regX.exec(pGraphs) )
+        ocr[gp[0]] = ocr.hasOwnProperty(gp[0]) ? ocr[gp[0]] + 1 : 1
+console.log(ocr)
+
+return ocr;
 }
 
-new Word().count("one two,three")
 module.exports=Word;
