b3caf1c191a612b1a39355661f432d1422cc28ec
Amit Kumar <amit00272@hotmail.com>
2017-02-21 16:31:05 +0530

    F

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -1,20 +1,20 @@
-
 var Word=function () {}
 
 Word.prototype.count = pGraphs => {
 
     var ocr={};
     pGraphs.toLowerCase()
-           .split(/[.?,¡¿!&@$%^:;\t\n\\\/ ]+/ig)
+           .split(/[.?,¡¿!&@$%^:;\t\n\\\/ ]+/)
            .map(val=>{
 
                if(val.trim()=='')return;
+
                val=val.replace(/\'(\w+)\'/,"$1");
-               ocr[val] =ocr.hasOwnProperty(val) ? ocr[val]+1:1;
+               ocr[val] = ocr.hasOwnProperty(val) ? ocr[val] + 1 : 1;
 
            });
 
-return ocr;
+    return ocr;
 }
 
 module.exports=Word;
